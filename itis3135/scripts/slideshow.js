509dfdaf5949ff6b0c20d0804de2482d447d3d8a
Abbad Chamma <abbadchamma@gmail.com>
2025-04-28 17:36:42 -0400

    Add slideshow with name-based images

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    let currentSlide = 0;
+    const slides = $('.slide');
+    const totalSlides = slides.length;
+
+    // Create thumbnails
+    slides.each(function(index) {
+        const img = $(this).find('img');
+        const thumbnail = $('<img>')
+            .attr('src', img.attr('src'))
+            .attr('alt', img.attr('alt'))
+            .addClass('thumbnail')
+            .click(function() {
+                showSlide(index);
+            });
+        $('.thumbnail-container').append(thumbnail);
+    });
+
+    // Show specific slide
+    function showSlide(n) {
+        slides.removeClass('active');
+        currentSlide = (n + totalSlides) % totalSlides;
+        slides.eq(currentSlide).addClass('active');
+    }
+
+    // Change slide
+    window.changeSlide = function(direction) {
+        showSlide(currentSlide + direction);
+    };
+
+    // Keyboard navigation
+    $(document).keydown(function(e) {
+        if (e.key === 'ArrowLeft') {
+            changeSlide(-1);
+        } else if (e.key === 'ArrowRight') {
+            changeSlide(1);
+        }
+    });
+
+    // Auto advance slides every 5 seconds
+    setInterval(function() {
+        changeSlide(1);
+    }, 5000);
+});
